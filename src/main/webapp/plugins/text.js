5c9ccf499c1d77ba80a080f4b536748681800027
ysawong <2761686+ysawong@users.noreply.github.com>
2020-03-10 23:56:07 -0400

    Update text.js

--- src/main/webapp/plugins/text.js
+++ src/main/webapp/plugins/text.js
@@ -6,10 +6,44 @@ Draw.loadPlugin(function(ui)
 	// Adds resource for action
 	mxResources.parse('extractText=Extract Text...');
 
+	
+	
+	
+	
 	// Adds action
 	ui.actions.addAction('extractText', function()
 	{
-		var dlg = new EmbedDialog(ui, ui.editor.graph.getIndexableText(),
+		var tmp = document.createElement('div');
+		var labels = [];
+		var label = '';
+		for (var key in this.model.cells)
+		{
+			var cell = this.model.cells[key];
+
+			if (this.model.isVertex(cell) || this.model.isEdge(cell))
+			{
+				if (this.isHtmlLabel(cell))
+				{
+					tmp.innerHTML = this.getLabel(cell);
+					label = mxUtils.extractTextWithWhitespace([tmp]);
+				}
+				else
+				{					
+					label = this.getLabel(cell);
+				}
+
+				label = mxUtils.trim(label.replace(/[\x00-\x1F\x7F-\x9F]|\s+/g, ' '));
+
+				if (label.length > 0)
+				{
+					labels.push(label);
+				}
+			}
+		}
+	
+// 		var dlg = new EmbedDialog(ui, ui.editor.graph.getIndexableText(),
+// 			null, null, null, 'Extracted Text:');
+		var dlg = new EmbedDialog(ui, labels.join(','),
 			null, null, null, 'Extracted Text:');
 		ui.showDialog(dlg.container, 440, 240, true, true);
 		dlg.init();
